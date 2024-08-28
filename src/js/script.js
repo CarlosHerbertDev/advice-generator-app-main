8cb85350c1de17ea2bf234280b23238d4cff028c
CarlosHerbertDev <hrbrtvieira@gmail.com>
2024-08-28 16:03:11 -0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+document.querySelector("button[class = 'button']").addEventListener('click', gerarNovoConselho)
+
+async function gerarNovoConselho(){
+    try {
+        const url = await fetch('https://api.adviceslip.com/advice')
+            if (!url.ok) {
+                throw new Error("Ops, parace que tivemos um erro com a resposta da API!");
+            }
+
+        const resp = await url.json()
+        const mensagemConselho = resp.slip.advice
+        const idMensagem = resp.slip.id
+        document.querySelector(".advice-of-day").innerText = mensagemConselho    
+        document.querySelector(".advice-number").innerText = `ADVICE #${idMensagem}`
+    } catch (error) {
+        console.error("Erro ao tentar buscar as informações da API", error);
+    }
+    
+}
+gerarNovoConselho()
